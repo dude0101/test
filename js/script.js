9ed9672b0c32f7fedaed3e22787b2e4186ed6b7b
dude <dude@mail.ru>
2021-12-23 18:42:21 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//let eyecolor = 'brown';
+//console.log(eyecolor);
+//let userProfile = {
+//	user: 'Faust',
+//	username: 'Faust',
+//	message: 'Go fuck yorself!'
+//}
+//console.log(userProfile);
+//
